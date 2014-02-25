0307b9e473786e099040287e50ec75dbe603a48b
David Brodsky <dbro@dbro.pro>
2014-02-24 17:31:50 -0800

    quick video capture test. test stops when CAPTURE_VIDEO Intent brings Camera application to foreground

--- src/info/guardianproject/mrapp/test/TestSimpleVideo.java
+++ src/info/guardianproject/mrapp/test/TestSimpleVideo.java
@@ -1,14 +1,24 @@
 package info.guardianproject.mrapp.test;
 
 import android.test.ActivityInstrumentationTestCase2;
+
 import com.google.android.apps.common.testing.ui.espresso.Espresso;
+import com.slidingmenu.lib.SlidingMenu;
+
 import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
 import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
-
-
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withClassName;
+import static org.hamcrest.Matchers.containsString;
 import info.guardianproject.mrapp.HomeActivity;
 import info.guardianproject.mrapp.R;
+import info.guardianproject.mrapp.test.viewactions.OpenSlidingMenuAction;
 
+/**
+ * Test creating a quick video recording via 
+ * the SlidingMenu's btnDrawerQuickCaptureVideo Button
+ * @author davidbrodsky
+ *
+ */
 public class TestSimpleVideo extends ActivityInstrumentationTestCase2<HomeActivity> {
 
 	public TestSimpleVideo() {
@@ -25,7 +35,16 @@ public class TestSimpleVideo extends ActivityInstrumentationTestCase2<HomeActivi
 	}
 	
 	public void test(){
-		//Espresso.onView(withId(R.id.newButton)).perform(click());
+		// Reveal SlidingMenu and click the video capture button
+		Espresso.onView(withClassName(containsString("SlidingMenu"))).perform(new OpenSlidingMenuAction());
+		Espresso.onView(withId(R.id.btnDrawerQuickCaptureVideo)).perform(click());
+		
+		// The OverlayCameraActivity appears. Click the overlay ImageView
+		Espresso.onView(withClassName(containsString("ImageView"))).perform(click());
+		
+		// The CAPTURE_VIDEO intent resolves. Unfortunately, 
+		// this ends the Espresso test and we can no longer test.
+		
 	}
 
 }
